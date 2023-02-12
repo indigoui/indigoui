6b8443692a31c81ca3805cbe3288b4e1be4bcd30
Paul Tanasă <pebly@tuta.io>
2023-02-12 14:03:29 +0200

    add(component): flag

---
+++ components/flag/flag.js
@@ -0,0 +1,11 @@
+[|% ../template : { ...context, attrs: ['name'], includeOnChange: true, includeOnConstructor: true } |]
+  #onConstructor() {
+    //this.#renderIcon();
+  }
+
+  #onChange(attr, newValue, oldValue) {
+    //const svg = this.#shadow.children[1];
+
+    //this.#shadow.removeChild(svg);
+  }
+[|end|]
